b5fa4d46fbfd865f069a0283ed7e7ca22971c32b
Lawrence Akpan <delaw34@yahoo.com>
2023-05-13 12:20:47 +0100

    Update readexcel.py
    change file name

--- bookreadexcel.py
+++ bookreadexcel.py
@@ -7,7 +7,6 @@ import sys
 #myfiles = open('phone_list.txt', 'r')
 myfiles = open(os.path.join(sys.path[0],'book_name.xls'), 'r')
 
-
 #read the file using the read function
 read_file = myfiles.read()
 
