4c476a652ee36483b44c959e65ec9c585cad41cc
Lawrence Akpan <delaw34@yahoo.com>
2023-05-13 12:19:13 +0100

    Update excelcreator.py

--- phoneexcel.py
+++ phoneexcel.py
@@ -3,14 +3,12 @@
 import os
 import sys
 
-
 #write a text file 
 book_name = ['merlin \n', 'buga \n', 'obidient \n', 'lion \n', 'python \n']
 book_name2 = ['movie \n', 'music\n', 'labour party \n', 'animal \n', 'software \n']
 book_name3 = ['things fall apart \n', 'micrco soft\n', 'google \n', 'amazon \n', 'the place \n']
 
 #write an excel file 
-
 with open(os.path.join(sys.path[0],'book_name.xls'), 'w') as book:
     book_name_write = book.writelines(book_name)
     book_name_write2 = book.writelines(book_name2)
