60cac6ab736d9be526e66d6db99a7771f3602fd4
Lawrence Akpan <delaw34@yahoo.com>
2023-05-13 11:58:49 +0100

    Update bookreadexcel.py
    added more decription

--- bookreadexcel.py
+++ bookreadexcel.py
@@ -1,7 +1,9 @@
+#to enable us access the file we import the os and sys modules
 import os
 import sys
 
 #open the file
+#kindly note if this open function on line 7 does not work then use line 8
 #myfiles = open('phone_list.txt', 'r')
 myfiles = open(os.path.join(sys.path[0],'book_name.xls'), 'r')
 
@@ -12,4 +14,3 @@ read_file = myfiles.read()
 print(read_file)
 
 myfiles.close()
-
